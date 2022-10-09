0efb8c90d80827381dcc4978c11d810cbb03ad61
dianhasnaramadhani.github.io <dianhasnaramadhani@gmail.com>
2022-10-09 19:15:59 +0700

    first upload

---
+++ DroidCafe2/app/src/main/java/lat/pam/droidcafe2/FirstFragment.java
@@ -0,0 +1,58 @@
+package lat.pam.droidcafe2;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+
+import androidx.navigation.fragment.NavHostFragment;
+
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        return inflater.inflate(R.layout.fragment_first, container, false);
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.donut).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast(getString(R.string.donut_order_message));
+            }
+        });
+
+        view.findViewById(R.id.ice_cream).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast(getString(R.string.ice_cream_order_message));
+            }
+        });
+
+        view.findViewById(R.id.froyo).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                displayToast(getString(R.string.froyo_order_message));
+            }
+        });
+
+
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getActivity(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+}
